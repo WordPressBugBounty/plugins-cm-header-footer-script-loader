66e327fa58c0441c04006a56fc3b9bafeca9d07e
Sean Murphy <sean@iamseanmurphy.com>
2025-03-25 10:18:18 +0000

    1.2.5

--- cm-header-footer-script-loader/js/admin.js
+++ cm-header-footer-script-loader/js/admin.js
@@ -178,6 +178,7 @@ jQuery(document).ready(function($) {
 				url: ajax_cm_handfsl_vars.ajaxurl,
 				data: {
 					action: 'cmhandfsl_create_update_rule',
+					nonce: ajax_cm_handfsl_vars.get_unique_id_nonce,
 					mode: 'create',
 					item_name: item_name.val(),
 					item_code: item_code.val(),
@@ -245,6 +246,7 @@ jQuery(document).ready(function($) {
 				url: ajax_cm_handfsl_vars.ajaxurl,
 				data: {
 					action: 'cmhandfsl_create_update_rule',
+					nonce: ajax_cm_handfsl_vars.get_unique_id_nonce,
 					mode: 'update',
 					id: rowkey,
 					item_name: item_name.val(),
@@ -277,6 +279,7 @@ jQuery(document).ready(function($) {
                 url: ajax_cm_handfsl_vars.ajaxurl,
                 data: {
                     action: 'cmhandfsl_delete_rule',
+					nonce: ajax_cm_handfsl_vars.get_unique_id_nonce,
                     id: rowkey,
                 },
                 method: "POST"
